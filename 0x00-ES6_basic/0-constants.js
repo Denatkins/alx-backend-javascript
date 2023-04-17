4600b899dbc9c2954d3f390b095fdab774b8182d
Denatkins <dennisodibo@yahoo.com>
2023-04-17 08:25:42 -0700

    Spec

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,17 +1,15 @@
-// Define a function that returns a string
-function taskOne() {
-  const sentence = 'I tend to use const whenever possible.';
+export function taskFirst() {
+  const sentence = 'I prefer const when I can.';
   return sentence;
 }
 
-// Define another function that returns a string
-function getEnd() {
-  return ' is also acceptable.';
+export function getLast() {
+  return ' is okay';
 }
 
-// Define a third function that concatenates two strings and returns the result
-function taskTwo() {
-  let phrase = 'However, there are times when ';
-  phrase += getEnd();
-  return phrase;
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
 }
