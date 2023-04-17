2ac1fee55b30b61f9af60ca34f508996df490bd6
Denatkins <dennisodibo@yahoo.com>
2023-04-17 08:35:56 -0700

    Spec

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export default function taskBlock(trueOrFalse) {
+  const sentence = false;
+  const setence2 = true;
+
+  if (trueOrFalse) {
+    const sentence = true;
+    const sentence2 = false;
+  }
+
+  return [sentence, setence2];
+}
